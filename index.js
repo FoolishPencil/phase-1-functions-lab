e936aa3bf62f2c8d74c0466a6caff5e6e1eff04c
daniel lance <poorfoolspencil@gmail.com>
2024-10-10 19:17:40 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    const hq = 42;
+    return Math.abs(distance - hq);
+}
+
+function distanceFromHqInFeet(distance) {
+    const blocks = distanceFromHqInBlocks(distance)
+    return blocks * 264;
+}
+
+function distanceTravelledInFeet(start, distance) {
+    const blocksTravelled = Math.abs(start - distance);
+    return blocksTravelled * 264;
+}
+
+function calculatesFarePrice(start, distance) {
+    const distanceInFeet = distanceTravelledInFeet(start, distance)
+
+    if (distanceInFeet <= 400) {
+        return 0;
+    }
+    else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+        return (distanceInFeet - 400) * 0.02;
+    }
+    else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+}
